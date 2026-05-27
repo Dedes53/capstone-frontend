083e7592f97b9b7ce228848311930a6c1c7832e6
Dedes <federico.lepore73@gmail.com>
2026-05-27 14:55:31 +0200

    Comments

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,50 +1,3 @@
-// import { useState, useEffect } from "react";
-// // import { NavLink } from "react-router-dom";
-
-// function NavbarComponent() {
-//     const [open, setOpen] = useState(false);
-
-//     useEffect(() => {
-//         const handleEsc = (e) => {
-//             if (e.key === "Escape") setOpen(false);
-//         };
-//         window.addEventListener("keydown", handleEsc);
-//         return () => window.removeEventListener("keydown", handleEsc);
-//     }, []);
-
-//     useEffect(() => {
-//         document.body.style.overflow = open ? "hidden" : "auto";
-//     }, [open]);
-
-//     return (
-//         <>
-//             <nav className="navbar">
-//                 <div className="logo">
-//                     <img src="/logo.png" alt="Logo" />
-//                 </div>
-
-//                 <div className="title">
-//                     <span className="skill">Skill</span><span className="swap">Swap</span>
-//                 </div>
-
-//                 <button
-//                     className={`menu-btn ${open ? "active" : ""}`}
-//                     onClick={() => setOpen(!open)}
-//                 >
-//                     <span></span><span></span><span></span>
-//                 </button>
-//             </nav>
-
-//             <div className={`overlay ${open ? "active" : ""}`} onClick={() => setOpen(false)} />
-
-
-//         </>
-//     );
-// }
-
-// export default NavbarComponent;
-
-
 import "../assets/css/NavbarComponent.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -68,7 +21,7 @@ function NavbarComponent() {
 
                 <Navbar.Toggle id="menubtn" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto gap-1 mt-3 mt-lg-0">
+                    <Nav className="ms-auto gap-1 mt-3 mt-lg-0">
                         {/* <Nav.Link as={NavLink} to="/">Home</Nav.Link> */}
                         <Nav.Link href="#contatti" className="nav-link-anchor">Contatti</Nav.Link>
                         <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
